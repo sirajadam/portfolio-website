bec18bb4ccea1d0b489d93bb7188c312a5a4138c
adam <adamsirajdeveloper@gmail.com>
2021-09-07 23:55:15 +0200

    added data folder with info about projects

---
+++ src/data/projects.js
@@ -0,0 +1,23 @@
+export const ProjectInfo = [
+  {
+    id: 1,
+    title: "Sorting Visualizer",
+    about:
+      "A website for visualizing different sorting algorithms such as Merge Sort, Quick Sort etc.",
+    tags: ["React", "JavaScript", "HTML", "CSS"],
+    demo: "www.github.io/dummy_link",
+    github: "link to my github",
+    image:
+      "https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+  },
+  {
+    id: 2,
+    title: "E-commerce website",
+    about: "A website for selling goods fast and easy",
+    tags: ["React", "TypeScript", "Go", "REST"],
+    demo: "www.github.io/dummy_link",
+    github: "link to my github",
+    image:
+      "https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+  },
+];
