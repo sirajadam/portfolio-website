d5c14762f2b51ca440423f1d12342e5d9db9ed43
adam <adamsirajdeveloper@gmail.com>
2021-09-08 15:19:06 +0200

    added Container to UI folder

---
+++ src/components/UI/HeaderTitle.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./HeaderTitle.module.css";
+
+const HeaderTitle = () => {
+  return (
+    <div className={styles["title"]}>
+      <h1>Projects</h1>
+      <div></div>
+    </div>
+  );
+};
+
+export default HeaderTitle;
