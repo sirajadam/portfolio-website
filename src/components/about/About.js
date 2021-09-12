c3d4dfb3ea10f5e077f3aabe138bf753ab670efe
adam <adamsirajdeveloper@gmail.com>
2021-09-12 21:04:06 +0200

    adding to github pages

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -34,7 +34,10 @@ const About = () => {
         </div>
       </div>
       <div className={styles["about-resume"]}>
-        <a href={require("../../assets/dude.png").default} download="dude.png">
+        <a
+          href={require("../../assets/CV.pdf").default}
+          download="Adam_Siraj_CV.pdf"
+        >
           <i class="fi-rr-cloud-download"></i>
           Download Resume
         </a>
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,30 @@
 export const ProjectInfo = [
   {
     id: 1,
-    title: "Sorting Visualizer",
-    about: "A website for visualizing different sorting algorithms",
-    tags: ["React", "JavaScript", "HTML", "CSS"],
-    demo: "https://github.com/sirajadam",
-    github: "https://github.com/sirajadam",
+    title: "Todolist",
+    about: "A minimalistic todolist",
+    tags: ["JavaScript", "HTML", "CSS"],
+    demo: "https://todolist-html-css-javascript.netlify.app/",
+    github: "https://github.com/sirajadam/todolist",
     image:
-      "https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      "https://raw.githubusercontent.com/sirajadam/todolist/main/preview.png",
   },
   {
     id: 2,
-    title: "E-commerce website",
-    about: "A website for selling goods fast and easy",
-    tags: ["React", "TypeScript", "Go", "REST"],
-    demo: "https://github.com/sirajadam",
-    github: "https://github.com/sirajadam",
-    image:
-      "https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    title: "Sorting Visualizer",
+    about: "A website for visualizing different sorting algorithms",
+    tags: ["React", "JavaScript", "HTML", "CSS"],
+    demo: "https://sirajadam.github.io/sorting-visualizer",
+    github: "https://github.com/sirajadam/sorting-visualizer",
+    image: "https://i.gyazo.com/99d9e7dbb1d404dbdc4f11616fb35aa5.png",
+  },
+  {
+    id: 3,
+    title: "WhatsApp Clone",
+    about: "A Front-End Clone of WhatsApp's desktop app ",
+    tags: ["React", "JavaScript", "Material UI", "HTML", "CSS"],
+    demo: "https://sirajadam.github.io/whatsapp-clone/",
+    github: "https://github.com/sirajadam/whatsapp-clone",
+    image: "https://i.gyazo.com/46199491d0ce553bafafa791b6d0a7a1.png",
   },
 ];
